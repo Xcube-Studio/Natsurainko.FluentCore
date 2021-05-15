5feeeaf09925b99c452269232cf1680f2598bfe9
xuan2006 <a-275@qq.com>
2021-05-15 14:38:29 +0800

    Add Project Files

---
+++ FluentCore.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using FluentCore.Extend.Service.Local;
+using FluentCore.Service.Local;
+using System;
+using System.Diagnostics;
+
+namespace FluentCore.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.Read();
+        }
+    }
+}
