6afa2297b924af033be866472cab132e98bcbae7
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2023-11-07 19:25:22 +0000

    Merge FluentCore.Manage project into FluentCore

---
+++ Natsurainko.FluentCore/Management/BaseModsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Nrk.FluentCore.Management;
+
+public abstract class BaseModsManager
+{
+    protected readonly string _modsFolder;
+
+    public BaseModsManager(string modsFolder)
+    {
+        _modsFolder = modsFolder;
+    }
+
+    public abstract IEnumerable<ModInfo> EnumerateMods();
+
+    public abstract void Switch(ModInfo modInfo, bool isEnable);
+
+    public abstract void Delete(ModInfo modInfo);
+}
---
+++ Natsurainko.FluentCore/Management/ModInfo.cs
@@ -0,0 +1,20 @@
+﻿using Nrk.FluentCore.GameResources.ModLoaders;
+
+namespace Nrk.FluentCore.Management;
+
+public record ModInfo
+{
+    public string AbsolutePath { get; set; }
+
+    public string DisplayName { get; set; }
+
+    public string Description { get; set; }
+
+    public string Version { get; set; }
+
+    public string[] Authors { get; set; }
+
+    public bool IsEnabled { get; set; }
+
+    public ModLoaderType[] SupportedModLoaders { get; set; }
+}
