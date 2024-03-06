7acfc489d6150d5285d140d65912e4daf9f0bf3f
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-03-06 21:51:37 +0000

    Add new offline account authenticator

--- Natsurainko.FluentCore/Authentication/Offline/DefaultOfflineAuthenticator.cs
+++ Natsurainko.FluentCore/Authentication/Offline/DefaultOfflineAuthenticator.cs
@@ -34,3 +34,20 @@ public class DefaultOfflineAuthenticator : IAuthenticator<OfflineAccount>
 
     #endregion
 }
+
+public class OfflineAuthenticator
+{
+    /// <summary>
+    /// Creates a new <see cref="DefaultOfflineAuthenticator"/> with the given name and UUID. UUID will be generated from <paramref name="name"/> if not provided.
+    /// </summary>
+    /// <param name="name">Name of the account</param>
+    /// <param name="uuid">UUID of the account (will be generated form <paramref name="name"/> if <see cref="null"/> is provided)</param>
+    /// <exception cref="ArgumentNullException">Throws if <paramref name="name"/> is null</exception>
+    public OfflineAccount Login(string name, Guid? uuid = null)
+        => new OfflineAccount
+        (
+            name ?? throw new ArgumentNullException(nameof(name)),
+            uuid ?? new Guid(MD5.HashData(Encoding.UTF8.GetBytes(name))),
+            Guid.NewGuid().ToString("N")
+        );
+}
