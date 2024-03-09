fff18835f064334ca51d641a74c1666a579be4d0
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-03-09 22:57:30 +0000

    Fix build

--- Natsurainko.FluentCore/Management/ModLoaders/QuiltInstaller.cs
+++ Natsurainko.FluentCore/Management/ModLoaders/QuiltInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Nrk.FluentCore.Management.Parsing;
+﻿using Nrk.FluentCore.Management.Downloader;
+using Nrk.FluentCore.Management.Parsing;
 using Nrk.FluentCore.Resources;
 using Nrk.FluentCore.Utils;
 using System;
