3bcac199b4a9c18a54cb3b72d5920080208ca3d1
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-03-12 09:59:11 +0000

    Add client.json deserialization test

---
+++ Samples/GameManagement/Program.cs
@@ -0,0 +1,17 @@
+﻿using Nrk.FluentCore.GameManagement;
+using System.Collections.Generic;
+using System.Text.Json;
+
+var mcFolderPath = @"C:\Users\jinch\Saved Games\Minecraft\.minecraft";
+var versions = Directory.GetDirectories(Path.Combine(mcFolderPath, "versions"));
+
+List<ClientJsonObject?> clientJsonObjects = new();
+foreach (var version in versions)
+{
+    string? clientJsonFile = Directory.GetFiles(version, "*.json").FirstOrDefault();
+    if (clientJsonFile == null)
+        continue;
+    string json = File.ReadAllText(clientJsonFile);
+    clientJsonObjects.Add(JsonSerializer.Deserialize<ClientJsonObject>(json));
+}
+Console.WriteLine();
