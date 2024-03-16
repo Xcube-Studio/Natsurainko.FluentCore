50101d526179ef1debf98084e2380b2f22828def
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-03-16 16:17:33 +0000

    Remove support for non-vanilla version id

--- Natsurainko.FluentCore/GameManagement_Experimental/MinecraftVersion.cs
+++ Natsurainko.FluentCore/GameManagement_Experimental/MinecraftVersion.cs
@@ -24,43 +24,29 @@ public enum MinecraftVersionType
 /// <param name="Type">Version ID</param>
 public record struct MinecraftVersion(string Version, MinecraftVersionType Type)
 {
-    private static Regex _releaseRegex = new(@"^\d+\.\d+(\.\d+)?");
-    private static Regex _preReleaseRegex = new(@"^\d+\.\d+(\.\d+)?-pre\d+");
-    private static Regex _snapshotRegex = new(@"^\d{2}w\d{2}[a-z]");
+    private static Regex _releaseRegex = new(@"^\d+\.\d+(\.\d+)?$");
+    private static Regex _preReleaseRegex = new(@"^\d+\.\d+(\.\d+)?-pre\d+$");
+    private static Regex _snapshotRegex = new(@"^\d{2}w\d{2}[a-z]$");
 
     /// <summary>
     /// Parse a version ID read from client.json into a <see cref="MinecraftVersion"/>
     /// </summary>
     /// <param name="id">ID in client.json (example: 1.19.3)
-    /// <para>Only matches the beginning, so modified versions such as 1.19.3-Fabric 0.14.12 are also supported</para>
     /// </param>
     /// <returns>The <see cref="MinecraftVersion"/> parsed</returns>
     public static MinecraftVersion Parse(string id)
     {
-        // Math release versions
-        var releaseMatch = _releaseRegex.Match(id);
-        if (releaseMatch.Success)
-            return new MinecraftVersion(releaseMatch.Value, MinecraftVersionType.Release);
-
-        // Match pre-release versions
-        var preReleaseMatch = _preReleaseRegex.Match(id);
-        if (preReleaseMatch.Success)
-            return new MinecraftVersion(preReleaseMatch.Value, MinecraftVersionType.PreRelease);
-
-        // Match snapshot versions
-        var snapshotMatch = _snapshotRegex.Match(id);
-        if (snapshotMatch.Success)
-            return new MinecraftVersion(snapshotMatch.Value, MinecraftVersionType.Snapshot);
-
-        // Match old beta versions
-        if (id.StartsWith("beta", StringComparison.OrdinalIgnoreCase))
+        if (_releaseRegex.IsMatch(id))
+            return new MinecraftVersion(id, MinecraftVersionType.Release);
+        else if (_preReleaseRegex.IsMatch(id))
+            return new MinecraftVersion(id, MinecraftVersionType.PreRelease);
+        else if (_snapshotRegex.IsMatch(id))
+            return new MinecraftVersion(id, MinecraftVersionType.Snapshot);
+        else if (id.StartsWith("beta", StringComparison.OrdinalIgnoreCase))
             return new MinecraftVersion(id, MinecraftVersionType.OldBeta);
-
-        // Match old alpha versions
-        if (id.StartsWith("alpha", StringComparison.OrdinalIgnoreCase))
+        else if (id.StartsWith("alpha", StringComparison.OrdinalIgnoreCase))
             return new MinecraftVersion(id, MinecraftVersionType.OldAlpha);
-
-        // Special versions or unknown versions
-        return new MinecraftVersion(id, MinecraftVersionType.Other);
+        else
+            return new MinecraftVersion(id, MinecraftVersionType.Other);
     }
 }
