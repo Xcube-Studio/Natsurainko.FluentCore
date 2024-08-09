5282d63ba7ce80a173b32e3fdbf082fbd85384e7
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-08-09 23:57:12 +0800

    Add dependency resolver

--- Natsurainko.FluentCore/Experimental/GameManagement/Downloader/IDownloader.cs
+++ Natsurainko.FluentCore/Experimental/GameManagement/Downloader/IDownloader.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 
 namespace Nrk.FluentCore.Experimental.GameManagement.Downloader;
 
 public interface IDownloader
 {
     IDownloadTask DownloadFileAsync(string url, string localPath, CancellationToken cancellationToken);
+
+    IDownloadTaskGroup DownloadFilesAsync(IEnumerable<(string url, string localPath)> files, CancellationToken cancellationToken);
 }
