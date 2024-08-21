8de8eb19d16edcc5c7543c2a373296f3b640d392
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-08-21 22:21:42 +0800

    Determine dependencies in DependencyResolver

--- Natsurainko.FluentCore/Experimental/GameManagement/Dependencies/DependencyResolver.cs
+++ Natsurainko.FluentCore/Experimental/GameManagement/Dependencies/DependencyResolver.cs
@@ -1,4 +1,5 @@
 ﻿using Nrk.FluentCore.Experimental.GameManagement.Downloader;
+using Nrk.FluentCore.Experimental.GameManagement.Instances;
 using Nrk.FluentCore.Utils;
 using System;
 using System.Collections.Concurrent;
@@ -13,16 +14,18 @@ namespace Nrk.FluentCore.Experimental.GameManagement.Dependencies;
 
 public class DependencyResolver
 {
-    public IReadOnlyList<MinecraftDependency> Dependencies { get; init; }
-
     public event EventHandler<(DownloadRequest, DownloadResult)>? DependencyDownloaded;
     public event EventHandler<IEnumerable<MinecraftDependency>>? InvalidDependenciesDetermined;
 
-    public static DependencyResolverBuilder CreateBuilder() => new();
+    private readonly List<MinecraftDependency> _dependencies = new();
+    private readonly MinecraftInstance _instance;
 
-    public DependencyResolver(IEnumerable<MinecraftDependency> deps)
+    public DependencyResolver(MinecraftInstance instance, IEnumerable<MinecraftDependency>? extraDependencies = null)
     {
-        Dependencies = new List<MinecraftDependency>(deps);
+        if (extraDependencies is not null)
+            _dependencies.AddRange(extraDependencies);
+
+        _instance = instance;
     }
 
     public async Task<GroupDownloadResult> VerifyAndDownloadDependenciesAsync(IDownloader? downloader = null, int fileVerificationParallelism = 10, CancellationToken cancellationToken = default)
@@ -32,13 +35,55 @@ public class DependencyResolver
 
         downloader ??= HttpUtils.Downloader;
 
-        // Verify
+        // TODO: 特殊情况处理
+        // 1. Forge端URL无法下载，需要替换
+        // 2. OptiFine部分依赖需要从安装器提取
+
+        // 1. Find all dependencies
+
+        // 1.1 Libraries
+        var (libs, nativeLibs) = _instance.GetRequiredLibraries();
+        _dependencies.AddRange(libs);
+        _dependencies.AddRange(nativeLibs);
+
+        if (_instance is ModifiedMinecraftInstance modInstance)
+        {
+            if (modInstance.HasInheritance)
+            {
+                (libs, nativeLibs) = modInstance.InheritedMinecraftInstance.GetRequiredLibraries();
+                _dependencies.AddRange(libs);
+                _dependencies.AddRange(nativeLibs);
+            }
+        }
+
+        // 1.2 Asset index
+        var assetIndex = _instance.GetAssetIndex();
+        bool isAssetIndexValid = await VerifyDependencyAsync(assetIndex, cancellationToken);
+        if (!isAssetIndexValid)
+        {
+            var result = await downloader.CreateDownloadTask(assetIndex.Url, assetIndex.FullPath).StartAsync();
+
+            if (result.Type == DownloadResultType.Failed)
+                throw new Exception("依赖材质索引文件获取失败");
+        }
+
+        // 1.3 Assets
+        var assets = _instance.GetRequiredAssets();
+        _dependencies.AddRange(assets);
+
+        // 1.3 Client Jar
+        var jar = _instance.GetJarElement();
+        if (jar != null)
+            _dependencies.Add(jar);
+
+        // 2. Verify dependencies
+
         // This is IO bound operation, using TPL is inefficient
         // TODO: Test performance of this implementation
         SemaphoreSlim semaphore = new(0, fileVerificationParallelism);
         ConcurrentBag<MinecraftDependency> invalidDeps = new();
 
-        var tasks = Dependencies.Select(async dep =>
+        var tasks = _dependencies.Select(async dep =>
         {
             await semaphore.WaitAsync(cancellationToken);
             try
@@ -60,7 +105,7 @@ public class DependencyResolver
         await Task.WhenAll(tasks);
         InvalidDependenciesDetermined?.Invoke(this, invalidDeps);
 
-        // Download
+        // 3. Download invalid dependencies
         var downloadItems = invalidDeps.Select(dep => new DownloadRequest(dep.Url, dep.FullPath));
         var groupRequest = new GroupDownloadRequest(downloadItems);
         groupRequest.SingleRequestCompleted += (request, result) =>
@@ -70,8 +115,7 @@ public class DependencyResolver
         return await downloader.DownloadFilesAsync(groupRequest, cancellationToken);
     }
 
-    // TODO: change to private
-    public static async Task<bool> VerifyDependencyAsync(MinecraftDependency dep, CancellationToken cancellationToken = default)
+    private static async Task<bool> VerifyDependencyAsync(MinecraftDependency dep, CancellationToken cancellationToken = default)
     {
         if (!File.Exists(dep.FullPath))
             return false;
@@ -81,21 +125,5 @@ public class DependencyResolver
         string sha1Str = BitConverter.ToString(sha1Bytes).Replace("-", string.Empty).ToLower();
 
         return sha1Str == dep.Sha1;
-    }
-}
-
-public class DependencyResolverBuilder
-{
-    private readonly List<MinecraftDependency> _deps = new();
-
-    public DependencyResolverBuilder AddDependencies(IEnumerable<MinecraftDependency> deps)
-    {
-        _deps.AddRange(deps);
-        return this;
-    }
-
-    public DependencyResolver Build()
-    {
-        return new DependencyResolver(_deps);
     }
 }
