ca17c5d0590afb1cd88df5b891e3eb0c7518697a
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-08-31 13:18:17 +0800

    Add source generation context for ClientJsonObject

---
+++ Natsurainko.FluentCore/GameManagement/MinecraftJsonSerializerContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using static Nrk.FluentCore.GameManagement.ClientJsonObject.ArgumentsJsonObject;
+
+namespace Nrk.FluentCore.GameManagement;
+
+[JsonSerializable(typeof(ClientJsonObject))]
+[JsonSerializable(typeof(ConditionalClientArgument<GameArgumentRule>))]
+[JsonSerializable(typeof(ConditionalClientArgument<ClientJsonObject.OsRule>))]
+[JsonSerializable(typeof(IEnumerable<string>))]
+internal partial class MinecraftJsonSerializerContext : JsonSerializerContext
+{
+}
