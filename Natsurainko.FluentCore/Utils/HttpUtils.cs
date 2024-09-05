9eaae816dfc35554810110352b0ebb248df920d0
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-09-05 19:50:50 +0800

    Clean up HttpUtils

--- Natsurainko.FluentCore/Utils/HttpUtils.cs
+++ Natsurainko.FluentCore/Utils/HttpUtils.cs
@@ -9,43 +9,12 @@ using System.Threading;
 
 namespace Nrk.FluentCore.Utils;
 
-#nullable disable
-// TODO: refactor downloading system later
-
 // TOOD: use internal
 public static class HttpUtils
 {
     public static readonly HttpClient HttpClient = new();
     public static readonly IDownloader Downloader = new MultipartDownloader(HttpClient, 1024 * 1024, 8, 64);
     public static readonly MemoryPool<byte> MemoryPool = MemoryPool<byte>.Shared;
-
-    public static HttpResponseMessage HttpGet(
-        string url,
-        Tuple<string, string> authorization = default,
-        HttpCompletionOption httpCompletionOption = HttpCompletionOption.ResponseContentRead)
-    {
-        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
-
-        if (authorization != null)
-            requestMessage.Headers.Authorization = new AuthenticationHeaderValue(
-                authorization.Item1,
-                authorization.Item2
-            );
-
-        var responseMessage = HttpClient.Send(requestMessage, httpCompletionOption, CancellationToken.None);
-
-        if (responseMessage.StatusCode.Equals(HttpStatusCode.Found))
-        {
-            string redirectUrl = responseMessage.Headers.Location.AbsoluteUri;
-
-            responseMessage.Dispose();
-            GC.Collect();
-
-            return HttpGet(redirectUrl, authorization, httpCompletionOption);
-        }
-
-        return responseMessage;
-    }
 
     public static string ReadAsString(this HttpContent content)
     {
