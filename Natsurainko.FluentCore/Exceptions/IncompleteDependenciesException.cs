406b8e581a64f4d9b4400fbb41572288dddb7368
natsurainko <a-275@qq.com>
2025-05-05 20:29:22 +0800

    详细列出 IncompleteDependenciesException 的错误消息

--- Natsurainko.FluentCore/Exceptions/IncompleteDependenciesException.cs
+++ Natsurainko.FluentCore/Exceptions/IncompleteDependenciesException.cs
@@ -1,18 +1,29 @@
 ﻿using Nrk.FluentCore.GameManagement.Downloader;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace Nrk.FluentCore.Exceptions;
 
 /// <summary>
 /// 依赖补全不完整错误
 /// </summary>
-public class IncompleteDependenciesException : Exception
+public class IncompleteDependenciesException(
+    IReadOnlyList<(DownloadRequest, DownloadResult)> failed, 
+    string message) : Exception(GenerateMessage(failed, message))
 {
-    public IReadOnlyList<(DownloadRequest, DownloadResult)> Failed { get; init; }
+    public IReadOnlyList<(DownloadRequest, DownloadResult)> Failed { get; init; } = failed;
 
-    public IncompleteDependenciesException(IReadOnlyList<(DownloadRequest, DownloadResult)> failed, string message) : base(message)
+    private static string GenerateMessage(IReadOnlyList<(DownloadRequest, DownloadResult)> failed, string message)
     {
-        Failed = failed;
+        StringBuilder stringBuilder = new();
+        stringBuilder.AppendLine(message);
+        stringBuilder.AppendLine(string.Empty);
+
+        foreach (var (request, result) in failed)
+            stringBuilder.AppendLine($"<{Path.GetFileName(request.LocalPath)}>[{request.Url}]: {result.Exception?.Message}");
+
+        return stringBuilder.ToString();
     }
 }
